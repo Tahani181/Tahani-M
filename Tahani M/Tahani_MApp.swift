ce5fa7cbfad03378d1670bb00ad28566e0769bbb
Tahani <Tahani.14192@gmail.com>
2024-10-20 15:37:02 +0300

    Initial Commit

---
+++ Tahani M/Tahani_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tahani_MApp.swift
+//  Tahani M
+//
+//  Created by Tahani Mohammed Alhawsawi on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Tahani_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
